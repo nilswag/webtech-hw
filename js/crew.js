b23a723eb2b7e6cdf58ce1e50a08e2b44c6ecb63
nilswag <nilswagemaker05@icloud.com>
2026-03-06 14:02:21 +0100

    Changed nodeName to tagName

--- js/crew.js
+++ js/crew.js
@@ -4,16 +4,15 @@ let teams = [];
 const playersSection = document.querySelector(".players__section");
 
 const selectOptions = document.querySelector(".crew-menus__select");
-const seen = new Set();
-const updateSelectOptions = (parent) => {
+const updateSelectOptions = (seen, parent) => {
     for (let node of parent.childNodes) {
-        if (node.nodeType !== Node.ELEMENT_NODE || seen.has(node.nodeName)) continue;
-        seen.add(node.nodeName);
-        if (node.childNodes.length != 0) updateSelectOptions(node);
+        if (node.nodeType !== Node.ELEMENT_NODE || seen.has(node.tagName)) continue;
+        seen.add(node.tagName);
+        if (node.childNodes.length != 0) updateSelectOptions(seen, node);
     
         let option = document.createElement("option");
-        option.value = node.nodeName;
-        option.innerText = node.nodeName;
+        option.value = node.tagName;
+        option.innerText = node.tagName;
     
         selectOptions.appendChild(option);
     }
@@ -30,7 +29,9 @@ fileReader.addEventListener("load", (e) => {
     players = json.players.map(Player.fromJSON);
     teams = json.teams.map(Team.fromJSON);
     players.forEach(p => Player.toHTML(p, playersSection));
-    updateSelectOptions(playersSection);
+
+    const seen = new Set();
+    updateSelectOptions(seen, playersSection);
 });
 
 const fileInput = document.getElementById("file-input");
@@ -48,5 +49,3 @@ fileInput.addEventListener("change", (e) => {
 
     fileReader.readAsText(file); 
 });
-
-updateSelectOptions(playersSection);
