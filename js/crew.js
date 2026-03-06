810e4445d8585699da534ae6aaaf8722a6d26e0d
J. Mostert <J.M.Mostert@students.uu.nl>
2026-03-06 20:41:08 +0100

    Made default HTML in JS

--- js/crew.js
+++ js/crew.js
@@ -1,6 +1,20 @@
-
+const main = document.getElementsByTagName("main")[0];
 let players = [];
 let teams = [];
+
+const heading = document.createElement("h1");
+heading.innerText = "Crew";
+main.appendChild(heading);
+
+const fileReaderInput = document.createElement("input");
+fileReaderInput.type = "file";
+fileReaderInput.id = "file-input";
+main.appendChild(fileReaderInput);
+
+const playerSection = document.createElement("section");
+playerSection.classList.add("players__section");
+main.appendChild(playerSection);
+
 
 const fileReader = new FileReader();
 fileReader.addEventListener("load", (e) => {
