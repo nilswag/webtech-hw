3b4d288f5301791b55e27d97c86e2c5a357d7ed8
nilswag <nilswagemaker05@icloud.com>
2026-03-13 13:26:55 +0100

    Removed styling menu from crew page

--- js/crew.js
+++ js/crew.js
@@ -3,49 +3,6 @@ let players = [];
 let teams = [];
 const html = document.querySelector("html");
 
-const optionsBlacklist = [
-    "meta",
-    "head",
-    "script",
-    "title",
-    "link"
-];
-
-const selectOptions = document.querySelector(".crew-menus__select");
-const updateSelectOptions = (seen, parent) => {
-    for (let node of parent.childNodes) {
-        if (node.nodeType !== Node.ELEMENT_NODE) continue;
-        const name = node.tagName.toLowerCase();
-        if (seen.has(name) || optionsBlacklist.includes(name)) continue;
-        seen.add(name);
-        if (node.childNodes.length != 0) updateSelectOptions(seen, node);
-    
-        let option = document.createElement("option");
-        option.value = name;
-        option.innerText = name;
-    
-        selectOptions.appendChild(option);
-    }
-};
-
-const applyButton = document.querySelector(".crew-menus__apply");
-applyButton.addEventListener("click", (e) => {    
-    if (!selectOptions.value) {
-        window.alert("Please select an element.");
-        return;
-    }
-    const fontSize = document.querySelector(".crew-menus__font-size");
-    const fontColor = document.querySelector(".crew-menus__font-color");
-    const backgroundColor = document.querySelector(".crew-menus__background-color");    
-
-    const tags = document.querySelectorAll(selectOptions.value);
-    tags.forEach(tag => {
-        if (fontSize) tag.style.setProperty("font-size", `${fontSize.value}px`, "important");
-        if (fontColor) tag.style.setProperty("color", fontColor.value, "important");
-        if (backgroundColor) tag.style.setProperty("background-color", backgroundColor.value, "important");
-    });
-});
-
 const heading = document.createElement("h1");
 heading.innerText = "Crew";
 main.appendChild(heading);
