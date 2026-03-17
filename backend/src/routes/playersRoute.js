d430e45bf8cc3868e93dfb3975b2170438b559c6
nilswag <nilswagemaker05@icloud.com>
2026-03-17 12:22:35 +0100

    Prepared routes

---
+++ backend/src/routes/playersRoute.js
@@ -0,0 +1,4 @@
+import express from "express";
+const router = new express.Router();
+
+router.get("/");
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,12 @@
-import * as setup from "../database/setup.js"
-setup.test();
+import express from "express";
+import * as setup from "../database/setup.js";
+for (let func of setup) {
+    
+}
 
-import { test } from "../database/queries/testQueries.js";
-const result = await test();
-console.log(result);
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`Listening to port ${port}.`);
+});
