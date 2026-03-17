8841f2c05339c23fdcbd986bac4c1abbb988a2dd
nilswag <nilswagemaker05@icloud.com>
2026-03-17 12:24:32 +0100

    Automatically setup all database DDL

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import * as setup from "../database/setup.js";
-for (let func of setup) {
-    
-}
+for (let setupFunc of Object.values(setup)) setupFunc();
 
 const app = express();
 const port = process.env.PORT || 3001;
