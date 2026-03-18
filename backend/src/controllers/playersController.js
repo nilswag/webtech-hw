e6d83abcd72de5e2dda6a7796144a5072e8f7605
nilswag <nilswagemaker05@icloud.com>
2026-03-18 14:00:16 +0100

    Finished controllers for the player service

--- backend/src/controllers/playersController.js
+++ backend/src/controllers/playersController.js
@@ -2,12 +2,20 @@ import * as service from "../services/playersService.js";
 
 export async function getPlayers(req, res) {
     try {
-        
+        const result = await service.getPlayers();
+        res.status(200).json(result);
     } catch (err) {
         return next(err);
     }
 };
 
 export async function getPlayer(req, res) {
-
+    try {
+        const result = await service.getPlayer(req.params.id);
+        if (result.length == 0)
+            return res.status(404).json({ message: `Player not found.` });
+        res.status(200).json(result);
+    } catch (err) {
+        return next(err);
+    }
 }
