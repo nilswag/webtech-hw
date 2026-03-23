f22b696390acfe43df6fd276cbe06d6a028982f8
nilswag <nilswagemaker05@icloud.com>
2026-03-23 12:15:36 +0100

    Small error fix

--- backend/src/middleware/loggingMiddleware.js
+++ backend/src/middleware/loggingMiddleware.js
@@ -1,13 +1,13 @@
-import { log, error } from "../util/logging.js";
+import * as logging from "../util/logging.js";
 
 export function log(req, res, next) {
-    log(`${req.method} request at ${req.url}`)
+    logging.log(`${req.method} request at ${req.url}`)
     next();
 };
 
 export function error(err, req, res, next) {
     const message = err.message || "Internal server error";
-    error(message);
+    logging.error(message);
     res.status(err.status || 500).json({
         message: message
     });
