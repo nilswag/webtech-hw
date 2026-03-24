4cd741a28dc57c7c0772390258fed6521b9c8ce2
nilswag <nilswagemaker05@icloud.com>
2026-03-24 11:35:21 +0100

    Added comments to playersQueries

--- backend/database/queries/playersQueries.js
+++ backend/database/queries/playersQueries.js
@@ -1,13 +1,29 @@
 import { fetchAll, runQuery } from "../database.js";
 
+/**
+ * Function to query all players.
+ * @returns A promise containing all players.
+ */
 export async function fetchPlayers() {
     return await fetchAll("SELECT * FROM Players;");
 }
 
+/**
+ * Function to query specific player.
+ * @param {*} id The id of the player.
+ * @returns A promise containing the player if found.
+ */
 export async function fetchPlayer(id) {
     return await fetchAll("SELECT * FROM Players WHERE id == ?;", id);
 }
 
+/**
+ * Function to add a player to database.
+ * @param {*} firstName First name of the player.
+ * @param {*} lastName Last name of the player.
+ * @param {*} age Age of the player.
+ * @returns An empty promise.
+ */
 export async function addPlayer(firstName, lastName, age) {
     return await runQuery("INSERT INTO Players(firstName, lastName, age) VALUES(?, ?, ?);", firstName, lastName, age);
 }
