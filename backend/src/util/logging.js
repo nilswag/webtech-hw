43a5d1d77c90800f03e5992d045fa1cf62ee36bd
nilswag <nilswagemaker05@icloud.com>
2026-03-24 11:39:19 +0100

    Added logging

--- backend/src/util/logging.js
+++ backend/src/util/logging.js
@@ -1,12 +1,24 @@
 
+/**
+ * A helper function to get a timestamp.
+ * @returns A formatted string of the current time.
+ */
 function timestamp() {
     return new Date().toTimeString().slice(0, 8);
 }
 
+/**
+ * Helper function to log a message.
+ * @param {*} msg The message to be logged.
+ */
 export function log(msg) {
     console.log(`[${timestamp()}] ${msg}`);
 }
 
+/**
+ * Helper function to log an error.
+ * @param {*} msg The error to be logged.
+ */
 export function error(msg) {
     console.error(`[${timestamp()}] Error: ${msg}`);
 }
