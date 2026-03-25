993a0a6a516d4a4d28eb30740daabee4d11b230a
Hommerion <hommelpot@gmail.com>
2026-03-25 15:42:17 +0100

    Links between pages work again

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -4,8 +4,12 @@ import __rootdirname from "../server.js";
 
 const router2 = express.Router();
 
-router2.get('/', (req, res) => {
-    res.sendFile(path.join(__rootdirname, "frontend/public/html/", "index.html"));
-});
+let ahahah = {"/": "index.html", "/about": "about.html", "/crew": "crew.html", "/history": "history.html", "/results": "results.html", "/americas_cup": "americas_cup.html"};
+
+for (const [key, value] of Object.entries(ahahah)) {
+    router2.get(key, (req, res) => {
+        res.sendFile(path.join(__rootdirname, "frontend/public/html/", value));
+    });
+}
 
 export default router2;
