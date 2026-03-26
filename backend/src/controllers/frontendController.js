15413f6bcea5f65615bfa79d67c27db45f4845d2
nilswag <nilswagemaker05@icloud.com>
2026-03-26 10:48:14 +0100

    Added prototype register page

--- backend/src/controllers/frontendController.js
+++ backend/src/controllers/frontendController.js
@@ -3,3 +3,7 @@ import { staticHtml } from "../../util/frontendUtil.js"
 export async function index(req, res) {
     res.status(200).sendFile(staticHtml("index.html"));
 }
+
+export async function register(req, res) {
+    res.status(200).sendFile(staticHtml("register.html"));
+}
