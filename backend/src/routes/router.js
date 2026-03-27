e08ce666934fca1c0d899e4ca91bcb42c965b0b3
Hommerion <hommelpot@gmail.com>
2026-03-27 13:22:47 +0100

    Replaced placeholder name for page link dictionary with a better name.

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -4,9 +4,16 @@ import __rootdirname from "../server.js";
 
 const router2 = express.Router();
 
-let ahahah = {"/": "index.html", "/about": "about.html", "/crew": "crew.html", "/history": "history.html", "/results": "results.html", "/americas_cup": "americas_cup.html"};
+let pageLinks = {
+    "/": "index.html", 
+    "/about": "about.html", 
+    "/crew": "crew.html", 
+    "/history": "history.html", 
+    "/results": "results.html", 
+    "/americas_cup": "americas_cup.html"
+};
 
-for (const [key, value] of Object.entries(ahahah)) {
+for (const [key, value] of Object.entries(pageLinks)) {
     router2.get(key, (req, res) => {
         res.sendFile(path.join(__rootdirname, "frontend/public/html/", value));
     });
