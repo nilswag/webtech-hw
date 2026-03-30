5b6c6a88bfea786b21eac8ccca433ab8857f2e80
nilswag <nilswagemaker05@icloud.com>
2026-03-30 11:39:40 +0200

    Improved polling

--- frontend/public/js/countdown.js
+++ frontend/public/js/countdown.js
@@ -1,30 +1,32 @@
+import { getData } from "./util/api.js";
+
+let nextGame = null;
+
+async function fetchNextGame() {
+    nextGame = await getData("/statistics/nextGame");
+}
+
 async function updateCountdown() {
+    if (!nextGame) return;
+
     const eventName = document.getElementById("event-countdown--name");
     const days = document.getElementById("event-countdown--units__days");
     const hours = document.getElementById("event-countdown--units__hours");
     const minutes = document.getElementById("event-countdown--units__minutes");
     const seconds = document.getElementById("event-countdown--units__seconds");
 
-    let nextGame;
-    let nextGameName;
-    let nextGameDate;
-    let currentTime = new Date();
-
-    let timeDelta;
-
     // Gets the name and date of the next game
-    nextGame = await fetch("/api/statistics/nextGame");
-    nextGame = await nextGame.json();
-    nextGameName = nextGame.name;
-    nextGameDate = new Date(nextGame.date);
+    const nextGameName = nextGame.name;
+    const nextGameDate = new Date(nextGame.date);
 
     // Calculates difference and transforms to correct unit
-    timeDelta = nextGameDate - currentTime;
+    const currentTime = new Date();
+    const timeDelta = nextGameDate - currentTime;
 
-    daysDelta = Math.floor(timeDelta/(24*60*60*1000));
-    hoursDelta = Math.floor(timeDelta/(60*60*1000)) % 24;
-    minutesDelta = Math.floor(timeDelta/(60*1000)) % 60;
-    secondsDelta = Math.floor(timeDelta/1000) % 60;
+    const daysDelta = Math.floor(timeDelta/(24*60*60*1000));
+    const hoursDelta = Math.floor(timeDelta/(60*60*1000)) % 24;
+    const minutesDelta = Math.floor(timeDelta/(60*1000)) % 60;
+    const secondsDelta = Math.floor(timeDelta/1000) % 60;
 
     // Change HTML text to correct values
     eventName.innerHTML = nextGameName;
@@ -34,5 +36,7 @@ async function updateCountdown() {
     seconds.innerText = (secondsDelta<10) ? `0${secondsDelta}` : secondsDelta;
 }
 
-updateCountdown();
-setInterval(updateCountdown, 1000)
+setInterval(updateCountdown, 1000);
+setInterval(fetchNextGame, 60000);
+
+fetchNextGame();
