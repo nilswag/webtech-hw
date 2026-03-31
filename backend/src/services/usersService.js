54a268b9a3def1062ce2cd12529f112205a87c84
nilswag <nilswagemaker05@icloud.com>
2026-03-31 21:53:43 +0200

    idk

--- backend/src/services/usersService.js
+++ backend/src/services/usersService.js
@@ -2,6 +2,21 @@ import * as queries from "../../database/queries/usersQueries.js";
 import bcrypt from "bcrypt";
 
 /**
+ * Service helper function to get user.
+ * @param {*} email Email of user.
+ * @returns User if found otherwise throws error.
+ */
+async function getUser(email) {
+    const result = await queries.getUser(email);
+    if (!result) {
+        const err = new Error("No user with that email.");
+        err.status = 404;
+        throw err;
+    }
+    return result;
+}
+
+/**
  * Service function to add user.
  * @param {*} firstName First name of user.
  * @param {*} lastName Last name of user.
@@ -31,9 +46,6 @@ export async function addUser(firstName, lastName, email, password) {
 export async function login(email, password) {
     const user = await getUser(email);
 
-    // TODO: add httpsecure cookie with session
-    // TODO: generate session
-
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
         const err = new Error("Invalid password.");
@@ -41,20 +53,8 @@ export async function login(email, password) {
         throw err;
     }
 
-    return user;
-}
+    // TODO: add httpsecure cookie with session
+    // TODO: generate session
 
-/**
- * Service function to get user.
- * @param {*} email Email of user.
- * @returns User if found otherwise throws error.
- */
-export async function getUser(email) {
-    const result = await queries.getUser(email);
-    if (!result) {
-        const err = new Error("No user with that email.");
-        err.status = 404;
-        throw err;
-    }
-    return result;
+    return user;
 }
