407935ce2482b1f18b2135d40f52ce6018630b91
nilswag <nilswagemaker05@icloud.com>
2026-04-01 17:57:39 +0200

    Added session model class

--- backend/src/models/Session.js
+++ backend/src/models/Session.js
@@ -1,4 +1,44 @@
 
 export class Session {
-       
+    #id;
+    #userId;
+    #token;
+    #expires;
+
+    constructor(id, userId, token, expires) {
+        this.#id = id;
+        this.#userId = userId;
+        this.#token = token;
+        this.#expires = expires;
+    }
+
+    get id() {
+        return this.#id;
+    }
+
+    get userId() {
+        return this.#userId;
+    }
+
+    get token() {
+        return this.#token;
+    }
+
+    get expires() {
+        return this.#expires;
+    }
+
+    static from(obj) {
+        const { 
+            id = null,
+            userId = null,
+            token = null,
+            expires = null,
+        } = obj || {};
+
+        if (!userId) throw new Error("Missing userId parameter.");
+        if (!token) throw new Error("Missing token parameter.");
+        if (!expires) throw new Error("Missing expires parameter.");
+        return new Session(id, userId, token, expires);
+    }    
 }
