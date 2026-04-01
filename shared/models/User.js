192a26e622a9eadb07bbea2959abafc2e23e9fb8
nilswag <nilswagemaker05@icloud.com>
2026-04-01 17:09:43 +0200

    Added user class

---
+++ shared/models/User.js
@@ -0,0 +1,39 @@
+
+export class User {
+    #firstName;
+    #lastName;
+    #email;
+    #password;
+
+    constructor(firstName, lastName, email, password) {
+        this.#firstName = firstName;
+        this.#lastName = lastName;
+        this.#email = email;
+        this.#password = password;
+    }
+
+    get firstName() {
+        return this.#firstName;
+    }
+
+    get lastName() {
+        return this.#lastName;
+    }
+
+    get email() {
+        return this.#email;
+    }
+
+    get password() {
+        return this.#password;
+    }
+
+    static from(obj) {
+        const { firstName, lastName, email, password } = obj;
+        if (!firstName) throw new Error("Missing firstname parameter.");
+        if (!lastName) throw new Error("Missing lastName parameter.");
+        if (!email) throw new Error("Missing email parameter.");
+        if (!password) throw new Error("Missing password parameter.");
+        return new User(firstName, lastName, email, password);
+    }
+}
