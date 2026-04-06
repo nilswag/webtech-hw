e78c8ebafbb20500d7c78ea563ca3ad678e0e604
nilswag <nilswagemaker05@icloud.com>
2026-04-06 17:39:32 +0200

    Added frontend handling of being logged in or out

---
+++ frontend/public/js/session.js
@@ -0,0 +1,22 @@
+
+let login = document.getElementById("header__navigation__login");
+let register = document.getElementById("header__navigation__register");
+let logout = document.getElementById("header__navigation__logout");
+
+function handler() {
+    // assuming we dont add extra cookies
+    if (document.cookie.includes("loggedIn=true")) {
+        register.style.display = "none";
+        login.style.display = "none";
+        logout.style.display = "block";
+    } else {
+        register.style.display = "block";
+        login.style.display = "block";
+        logout.style.display = "hidden";
+    }
+}
+handler();
+
+login.addEventListener("click", handler);
+register.addEventListener("click", handler);
+logout.addEventListener("click", handler);
