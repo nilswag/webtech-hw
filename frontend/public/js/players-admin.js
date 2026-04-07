1ad0b755cedd73102acb5ee8d63ba1d3fe5c2984
nilswag <nilswagemaker05@icloud.com>
2026-04-07 20:01:05 +0200

    Fixed more paths

--- frontend/public/js/players-admin.js
+++ frontend/public/js/players-admin.js
@@ -2,7 +2,7 @@ import { PlayerCard } from "./cards.js";
 
 async function showPlayers() {
     const playersList = document.getElementById("players__list");
-    let emptyPlayer = new PlayerCard(0, "../media/images/portraits/empty-image.jpg", true, "??", "??");
+    let emptyPlayer = new PlayerCard(0, "media/images/portraits/empty-image.jpg", true, "??", "??");
 
     let emptyPlayerCard = emptyPlayer.createPlayerCard();
     playersList.appendChild(emptyPlayerCard);
--- frontend/public/js/teams-admin.js
+++ frontend/public/js/teams-admin.js
@@ -3,7 +3,7 @@ import { TeamCard } from "./cards.js";
 async function showTeams() {
     const teamsList = document.getElementById("teams__list");
 
-    let emptyTeam = new TeamCard(0, "../media/images/portraits/empty-image.jpg", true, "??");
+    let emptyTeam = new TeamCard(0, "media/images/portraits/empty-image.jpg", true, "??");
     let emptyTeamCard = emptyTeam.createTeamCard();
 
     teamsList.appendChild(emptyTeamCard);
