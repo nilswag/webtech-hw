4032e521cc9a6f2ce19155cd3cba89d0bf86ba03
J. Mostert <J.M.Mostert@students.uu.nl>
2026-04-10 23:35:01 +0200

    edit teams

--- backend/database/queries/teamsQueries.js
+++ backend/database/queries/teamsQueries.js
@@ -12,18 +12,14 @@ export async function getTeam(id) {
     return await fetchAll("SELECT * FROM Teams WHERE id = ?", id);
 }
 
-export async function addTeam(name, image, wins, losses, totalPoints) {
-    return await runQuery("INSERT INTO Teams(name, image, wins, losses, totalPoints) VALUES(?, ?, ?, ?, ?)", name, image, wins, losses, totalPoints);
+export async function addTeam(name, image) {
+    return await runQuery("INSERT INTO Teams(name, image, wins, losses, totalPoints) VALUES(?, ?, 0, 0, 0)", name, image);
 }
 
 export async function deleteTeam(id) {
-    return await runQuery("DELETE FROM Team WHERE id = (?);", team);
+    return await runQuery("DELETE FROM Teams WHERE id = (?);", id);
 }
 
-export async function updateTeamName(id, name) {
-    return await runQuery("UPDATE Team SET name = (?) WHERE id = (?)", name, id);
-}
-
-export async function updateTeamImage(id, image) {
-    return await runQuery("UPDATE Team SET image = (?) WHERE id = (?);", image, id);
+export async function updateTeam(id, name, image) {
+    return await runQuery("UPDATE Teams SET name = ?, image = ? WHERE id = ?;", name, image, id);
 }
