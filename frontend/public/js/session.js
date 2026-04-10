50becfba5d5e2c2a98f4595ac98ba937b8f62645
nilswag <nilswagemaker05@icloud.com>
2026-04-10 15:38:09 +0200

    Profile button dissapears when not logged in

--- frontend/public/js/session.js
+++ frontend/public/js/session.js
@@ -2,6 +2,7 @@
 let login = document.getElementById("header__navigation__login");
 let register = document.getElementById("header__navigation__register");
 let logout = document.getElementById("header__navigation__logout");
+let profile = document.getElementById("header__navigation__profiel");
 
 function handler() {
     // assuming we dont add extra cookies
@@ -9,10 +10,13 @@ function handler() {
         register.style.display = "none";
         login.style.display = "none";
         logout.style.display = "block";
+        profile.style.display = "block";
+
     } else {
         register.style.display = "block";
         login.style.display = "block";
-        logout.style.display = "hidden";
+        logout.style.display = "none";
+        profile.style.display = "none";
     }
 }
 handler();
