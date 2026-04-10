d96517880ad2aebbc091e1931f8ca6d689a27858
nilswag <nilswagemaker05@icloud.com>
2026-04-10 12:49:42 +0200

    Added files for auth service and middleware

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,4 @@
+
+export async function auth(req, res, next) {
+    
+}
---
+++ backend/src/services/authService.js
@@ -0,0 +1,7 @@
+import bcrypt from "bcrypt";
+import { getSessions } from "./sessionsService";
+
+export async function isAdmin(token) {
+    const sessions = await getSessions();
+    const session = sessions.find(s => bcrypt.compare(token, s.token));
+}
