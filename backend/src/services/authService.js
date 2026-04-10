8c8e8e3bc8dbc1b491974c46a36a7f3991f888fa
nilswag <nilswagemaker05@icloud.com>
2026-04-10 12:51:29 +0200

    Added admins table

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -4,4 +4,6 @@ import { getSessions } from "./sessionsService";
 export async function isAdmin(token) {
     const sessions = await getSessions();
     const session = sessions.find(s => bcrypt.compare(token, s.token));
+
+    
 }
